920392a016afd5de3d85e6903e7d69f1582c8836
Shubham Sharma <xubham@amazon.com>
2022-06-09 10:59:37 -0700

    trigger build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          11 >  Edit <code>src/App.js</code> and save to reload.
+          12 >  Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
